1a9dfce83505195d8001429eaab4f95c2fa48516
akhil <akhil.munna02@gmail.com>
2016-06-24 23:20:56 +0530

    Recepie for Accordian Layout

---
+++ ExtJS CookBook/Chapter3/Layouts/Accordian.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+Ext.onReady(function () {
+
+    Ext.create('Ext.panel.Panel', {
+        title: 'Accordian Layout',
+        width: 350,
+        height: 400,
+        layout: 'accordion',
+        renderTo: Ext.getBody(),
+        items: [
+            {
+                title: 'Naruto Uzumaki',
+                html: 'Naruto Uzumaki'
+            },
+            {
+
+                title: 'Sasuke Uchicha',
+                html: 'Sasuke Uchicha'
+            },
+            {
+
+                title: 'Madara Uchicha',
+                html: 'Madara Uchicha'
+            }
+        ]
+       
+
+    });
+
+});
