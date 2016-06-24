cc7cd22f458fadc321bc80d101242a316e7d56b2
akhil <akhil.munna02@gmail.com>
2016-06-24 23:04:47 +0530

    ExtJS CookBook Recepies

---
+++ ExtJS CookBook/Chapter2/ManipulatingDOM/ExtGet.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="c:\users\akhil\documents\visual studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+Ext.onReady(function () {
+    var bookTitleEl = Ext.get('book-title');
+    alert('Book Title ID: ' + bookTitleEl.id);
+    bookTitleEl.hide();
+})
---
+++ ExtJS CookBook/Chapter2/ManipulatingDOM/ExtSelect.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="c:\users\akhil\documents\visual studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+Ext.onReady(function () {
+    var authors = Ext.select('#authors li');
+    authors.hide();
+})
