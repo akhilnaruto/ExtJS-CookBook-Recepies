3e8c14e21602007b54f18f5407bdd02284df8223
akhil <akhil.munna02@gmail.com>
2016-06-26 22:36:22 +0530

    started recepie on tree events

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/Tree/Tree_Events.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+var store = Ext.create('Ext.data.TreeStore', {
+    root: {
+        expanded: true,
+        children: [
+            {
+                text: 'Menu Item A',
+                leaf:true
+            },
+             {
+                 text: 'Menu Item B',
+                 leaf: true
+             },
+             {
+                 text: 'Menu Item C',
+                 leaf: true
+             },
+             {
+                 text: 'Menu Item D',
+                 leaf: true
+             },
+        ]
+    }
+});
+
+var westPanel = Ext.create('Ext.tree.Panel', {
+
+    title: 'Application Menu',
+    region: 'west',
+    margins: '0 5 0 0',
+    width: 200,
+    store: store,
+    rootVisible: false,
+    listeners: {
+        itemclick:function(tree,record,item,index,
+            e, options) {
+
+            var nodeText = record.data.text;
+            var tabPanel = viewport.items.get(1);
+            var tabBar = tabPanel.getTabBar();
+            var tabIndex;
+
+
+        }
+    }
+});
