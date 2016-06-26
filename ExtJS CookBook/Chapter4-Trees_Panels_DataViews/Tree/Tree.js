f21ca1814188cded0950f8e4d98494450731edc9
akhil <akhil.munna02@gmail.com>
2016-06-26 17:51:52 +0530

    Recepie for Tree Panel with sorting enabled

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/Tree/Tree.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+var store = Ext.create('Ext.data.TreeStore', {
+    proxy: {
+        type: 'ajax',
+        url: 'treeData.json'
+    },
+    root: {
+        text: 'Contries',
+        expanded: true
+    },
+    sorters: {
+        property: 'text',
+        direction: 'ASC' ////for descending change to 'DESC'
+    }
+});
+
+Ext.onReady(function () {
+    Ext.create('Ext.tree.Panel', {
+        title: 'Countries &amp; Cities',
+        width: 500,
+        height: 300,
+        store: store,
+        rootVisible: true, // hides main root node
+        renderTo: Ext.getBody(),
+        style:'margin 50px'
+    });
+
+});
