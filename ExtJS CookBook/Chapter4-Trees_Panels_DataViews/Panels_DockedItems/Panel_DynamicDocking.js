2b1fb5c159162e0d3f769dedf14287f3bd4d671d
akhil <akhil.munna02@gmail.com>
2016-07-01 22:35:40 +0530

    Recepie - Panel with Dynamic Docking

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/Panels_DockedItems/Panel_DynamicDocking.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+Ext.onReady(function () {
+
+  var panel =  Ext.create('Ext.panel.Panel', {
+
+        title: 'Panel With Dynamic Docking',
+        height: 300,
+        width: 400,
+        html: 'Panel With Dynamic Docking',
+        bodyPadding: 10,
+        renderTo: Ext.getBody()
+  });
+
+  panel.addDocked({
+      dock: 'bottom',
+      xtype: 'toolbar',
+      items: [
+          '->',
+          {
+              xtype: 'button',
+              text:'click me'
+          }
+      ]
+  });
+});
