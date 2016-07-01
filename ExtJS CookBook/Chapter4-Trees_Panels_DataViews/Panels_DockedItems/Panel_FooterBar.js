c6fe15dee60e4bec98ac2e31963dd87eaa9c47a2
akhil <akhil.munna02@gmail.com>
2016-07-01 22:24:09 +0530

    Recepie- Panle with fbar

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/Panels_DockedItems/Panel_FooterBar.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+Ext.onReady(function () {
+
+    Ext.create('Ext.panel.Panel', {
+        title: 'Panel With Fbar',
+        bodyPadding:20,
+        height: 400,
+        width: 300,
+        html: 'Panel with Footer bar',
+        fbar: [
+            'Footer Bar', '->',
+            {
+                xtype: 'button',
+                text: 'fbar button'
+            }
+        ],
+        renderTo: Ext.getBody(),
+        style: 'margin 50px'
+    });
+});
