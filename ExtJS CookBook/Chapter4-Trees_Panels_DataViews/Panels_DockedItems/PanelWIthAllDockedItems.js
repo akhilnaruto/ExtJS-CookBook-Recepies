8c14625fd5c33f7c357b57fa51c93a33436fae49
akhil <akhil.munna02@gmail.com>
2016-07-01 22:31:02 +0530

    Recepie - Panel With Docked Items on All Sides

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/Panels_DockedItems/PanelWIthAllDockedItems.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+Ext.onReady(function () {
+
+    Ext.create('Ext.panel.Panel', {
+
+        title: 'Panel With Docked Items in All Sides',
+        width: 500,
+        height: 300,
+        html: 'Panle With Docked Items in All Sides',
+        bodyPadding: 10,
+        lbar: ['lbar'],
+        tbar: ['tbar'],
+        rbar: ['rbar'],
+        bbar: ['bbar'],
+        renderTo: Ext.getBody(),
+        style:'margin 50px'
+
+    });
+
+});
