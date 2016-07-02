d8a04a577e3a8703411e87c034034893e5bf410b
akhil <akhil.munna02@gmail.com>
2016-07-02 23:22:09 +0530

    Recepie - tabpanel with tooltip

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/TabbedPanel.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+
+Ext.tip.QuickTipManager.init();
+
+Ext.onReady(function () {
+
+    Ext.create('Ext.tab.Panel', {
+
+        title: 'Tabbed Panel',
+        bodyPadding: 10,
+        height: 200,
+        width: 500,
+        items: [
+            {
+                title: 'item1'
+            },
+            {
+                title: 'item2',
+                tabConfig: {
+                    tooltip:'Tab two Tool tip Text'
+                }
+            }
+        ],
+        renderTo: Ext.getBody(),
+        style: 'margin 50px'
+    });
+
+});
