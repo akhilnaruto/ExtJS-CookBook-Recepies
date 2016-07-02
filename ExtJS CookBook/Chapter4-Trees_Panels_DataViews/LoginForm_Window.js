58899ff75c0553e02188f2599c6960ca72d295a8
akhil <akhil.munna02@gmail.com>
2016-07-02 23:09:43 +0530

    Recepie- Simple login Form

---
+++ ExtJS CookBook/Chapter4-Trees_Panels_DataViews/LoginForm_Window.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="C:\Users\akhil\Documents\Visual Studio 2015\Projects\ExtJS CookBook\ExtJS CookBook\ExtJS/ext-all-debug.js" />
+
+var form = Ext.create('Ext.form.Panel', {
+    bodyPadding: 10,
+    border: false,
+    defaultType: 'textfield',
+    items: [
+        {
+            fieldLabel: 'Username',
+            name: 'username',
+            allowBlank: false
+        },
+        {
+            fieldLabel: 'password',
+            name: 'password',
+            inputType: 'password',
+            allowBlank: false
+        }
+    ],
+    buttons: [
+        {
+            text: 'Login',
+            formBind: true,
+            disabled: true,
+            handler: function () {
+                alert('Login Button Clicked')
+            }
+
+        }
+    ]
+
+});
+
+
+
+
+Ext.onReady(function () {
+
+    Ext.create('Ext.window.Window', {
+
+        title: 'Login Window',
+        height: 140,
+        width: 300,
+        layout: 'fit',
+        items:[form]
+    }).show();
+});
